adfa607b413539d0e0bd191de1f435b4f0d9ffd4
ryanr4057 <ryanr4057@gmail.com>
2023-01-26 10:49:06 -0300

    fim

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,6 @@
 class Cliente < ApplicationRecord
+  has_many :vendas
+
   validates :nome, length: { minimum: 4, message: ":insira pelo menos 2 nomes" }
   validates :cpf, length: { minimum: 14, message: "inválido" }
   validates :idade, numericality: { greater_than_or_equal_to: 18, message: ":somente maiores de 18 anos",  only_integer: true }
